3a7d9fac358d6f03af281dfabdac8cba3c2ffc22
Kay Hannay <khannay@psi.de>
2010-01-29 11:24:42 +0100

    Add content encoding to rezepte.php

--- php/rezepte/rezepte.php
+++ php/rezepte/rezepte.php
@@ -4,9 +4,12 @@ require_once("checklogin.inc.php");
 require_once("dbinterface.inc.php");
 /* session_register("$recipes"); */
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title>Rezeptdatenbank</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 </head>
 <body>
 
