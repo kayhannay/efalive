98410d59a9fc73f9a313c480057d90e11449ca08
Kay <klinux@hannay.de>
2010-07-25 15:02:48 +0200

    Improve layout of WSVD template

--- web/templates/wsvd/index.php
+++ web/templates/wsvd/index.php
@@ -2,22 +2,59 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
     <head>
-        <!-- <jdoc:include type="head" /> -->
+        <jdoc:include type="head" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/wsvd/css/template.css" type="text/css" />
-        <?php 
+        <?php
             $home = 0;
             $menuitem = & JSite::getMenu();
             if ($menuitem->getActive() == $menuitem->getDefault()) {
                 $home = "1";
             }
         ?>
-        <?php if ($home): ?>
+        <?php if (($home) || ($this->countModules('left') == 0)): ?>
             <style type="text/css">
                 .content_background {padding-left:0;}
             </style>
         <?php endif; ?>
+        <script type="text/javascript">
+        <!--
+            function getElementByClass(className) {
+                var allHTMLTags = document.getElementsByTagName("li");
+                var length = allHTMLTags.length;
+                for (var i=0; i < length; i++) {
+                    if (allHTMLTags[i].className.indexOf(className) != -1) {
+                        return allHTMLTags[i];
+                    }
+                }
+            }
+            function moveSubMenu(element) {
+                var liElements = element.getElementsByTagName("span");
+                for (var i = 0; i < liElements.length; i++) {
+                    liElements[i].style.fontWeight = "bold";
+                }
+                var browser = navigator.appName;
+                var verString = navigator.appVersion;
+                var verIndex = verString.indexOf("MSIE ");
+                if(verIndex != -1) {
+                    version = verString.substring(verIndex + 5, verIndex + 6);
+                } 
+                var paddingX = element.offsetLeft;
+                var subMenu = document.getElementById('sub_menu');
+                subMenu.style.padding = "0px " + paddingX + "px";
+            }
+
+            function positionSubMenu() {
+                //print("In positionSubMenu()");
+                var parentString = "parent active ";
+                var element = getElementByClass(parentString);
+                if(element != null) {
+                    moveSubMenu(element);
+                }
+            }
+        -->
+        </script>
     </head>
     <body>
         <div class="page">
@@ -27,54 +64,22 @@
                 <div class="title2">Rudergesellschaft von 1893 e.V.</div>
             </div>
             <div class="menu_container">
-                <div class="menu_left">
-                    <div class="menu_right">
                         <div id="main_menu">
                             <jdoc:include type="modules" name="user3" />
                         </div>
                         <div id="sub_menu">
                             <script type="text/javascript">
                             <!--
-                                function getElementByClass(className) {
-                                    var allHTMLTags = document.getElementsByTagName("li");
-                                    var length = allHTMLTags.length;
-                                    for (var i=0; i < length; i++) {
-                                        if (allHTMLTags[i].className.indexOf(className) != -1) {
-                                            return allHTMLTags[i];
-                                        }
-                                    }
-                                }
-                                function positionSubMenu(element) {
-                                    var liElements = element.getElementsByTagName("span");
-                                    for (var i = 0; i < liElements.length; i++) {
-                                        liElements[i].style.fontWeight = "bold";
-                                    }
-                                    var browser = navigator.appName;
-                                    var verString = navigator.appVersion;
-                                    var verIndex = verString.indexOf("MSIE ");
-                                    if(verIndex != -1) {
-                                        version = verString.substring(verIndex + 5, verIndex + 6);
-                                    } 
-                                    var paddingX = element.offsetLeft - 29;
-                                    if((browser.indexOf("Explorer") != -1) && (version < 8)) {
-                                        paddingX += 19;
-                                    }
-                                    //document.write("Padding X: " + paddingX);
-                                    //paddingX = document.getElementById('current').offsetLeft - 27;
-                                    var subMenu = document.getElementById('sub_menu');
-                                    subMenu.style.padding = "0px " + paddingX + "px";
-                                }
-                                var parentString = "parent active ";
-                                var element = getElementByClass(parentString);
-                                if(element != null) {
-                                    positionSubMenu(element);
-                                }
+                                //print("Try to position...");
+                                positionSubMenu();
                             -->
                             </script>
                             <jdoc:include type="modules" name="user4" />
                         </div>
-                    </div>
-                </div>
+                <div class="corner_top_left"></div>
+                <div class="corner_top_right"></div>
+                <div class="corner_bottom_left"></div>
+                <div class="corner_bottom_right"></div>
             </div>
             <div class="content_area">
                 <?php if (! $home) : ?>
