36fa989f1c381f6921645ee09fbce4d1e3ec6421
Kay Hannay <klinux@hannay.de>
2010-09-27 21:35:24 +0200

    Fix cron backup email template

--- live/efa/config/chroot_local-includes/opt/efalive/templates/cron/email_backup.sh
+++ live/efa/config/chroot_local-includes/opt/efalive/templates/cron/email_backup.sh
@@ -3,10 +3,10 @@
 RECIPIENT="user@example.local"
 
 mkdir /tmp/efa_backup
-QUIET=1 /opt/efalive/bin/run_backup /tmp/efa_backup
+QUIET=1 /opt/efalive/bin/run_backup.sh /tmp/efa_backup
 for i in `ls /tmp/efa_backup`
 do
-    (/bin/echo "Automatic efa backup"; uuencode /tmp/efa_backup/$i $i) | mail -s"efa backup `/bin/date +%d.%m.%Y %k:%M:%S`" -t"$RECIPIENT"
+    (/bin/echo "Automatic efa backup"; uuencode /tmp/efa_backup/$i $i) | mail -s"efa backup `/bin/date '+%d.%m.%Y %k:%M:%S'`" -t "$RECIPIENT"
 done
 rm -r /tmp/efa_backup
 
