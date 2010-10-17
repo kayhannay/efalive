836aa6e04a8dd7b3141b1ceabef935bcfc70311e
Kay Hannay <klinux@hannay.de>
2010-10-17 22:09:35 +0200

    Fix rights hook; Adapt rights for cron backup

--- live/efa/config/chroot_local-hooks/03-rights.sh
+++ live/efa/config/chroot_local-hooks/03-rights.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 chown -R 1000:1000 /opt/efa/*
-chown -R 1000:1000 /opt/efa/.ivman
-chown -R 1000:1000 /opt/efa/.config
+chown -R 1000:1000 /home/efa/.ivman
+chown -R 1000:1000 /home/efa/.config
 chown 1000:1000 /home/efa/.xinitrc
 #chmod 644 /etc/ivman/IvmConfigActions.xml
 chmod 440 /etc/sudoers
