7e8657e22748f7fa742ddc4b647a60ff8bc960ab
Kay Hannay <klinux@hannay.de>
2013-07-03 22:14:18 +0200

    -Improve autobackup script to be quiet with option -q -Add man pages to debian package

--- content/usr/lib/efalive/bin/autobackup.sh
+++ content/usr/lib/efalive/bin/autobackup.sh
@@ -15,15 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright 2008-2012 Kay Hannay <klinux@hannay.de>
+# Copyright 2008-2013 Kay Hannay <klinux@hannay.de>
 #
 ###
 #
 # Automatic backup of efa data to an USB stick
-# Usage: autobackup.sh <DEVICE> [DELAY]
+# Usage: autobackup.sh [-q] [-d DELAY] <DEVICE>
 #
 # Where DEVICE is a mountable device and DELAY the time this script should
-# wait before it starts the backup in seconds.
+# wait before it starts the backup in seconds. With -q you can supress the 
+# beep sound that is normally played and the dialog for an error or success.
 #
 
 /bin/date
@@ -36,39 +37,80 @@ fi
 BEEP_ERROR="/usr/bin/beep -f 2000 -r 5 -d 50 -l 1000"
 BEEP_SUCCESS="/usr/bin/beep -f 1000 -r 3 -d 50"
 
+QUIET="FALSE"
 AUTO_USB_BACKUP_DIALOG="TRUE"
+
+function inform_success {
+	/bin/echo "$1"
+    	if [ "x$AUTO_USB_BACKUP_DIALOG" = "xTRUE" ]
+    	then
+        	/usr/bin/zenity --info --text="Autobackup: $1"
+    	fi
+	if [ "x$QUIET" = "xFALSE" ]
+	then
+		$BEEP_SUCCESS
+	fi
+}
+
+function inform_error {
+	/bin/echo "$1"
+    	if [ "x$AUTO_USB_BACKUP_DIALOG" = "xTRUE" ]
+    	then
+        	/usr/bin/zenity --error --text="Autobackup: $1"
+    	fi
+	if [ "x$QUIET" = "xFALSE" ]
+	then
+		$BEEP_ERROR
+	fi
+}
+
 export DISPLAY=:0.0
 if [ -f ~/.efalive/settings.conf ]
 then
     . ~/.efalive/settings.conf
 fi 
 
+while getopts :hd:q opt; do
+  case $opt in
+	q)
+		QUIET="TRUE"
+		AUTO_USB_BACKUP_DIALOG="FALSE"
+		;;
+	d)
+		if [[ $OPTARG =~ ^[0-9]+ ]]
+		then
+			/bin/echo "Wait for $OPTARG seconds ..."
+			/bin/sleep $OPTARG
+		else
+			inform_error "Error, specified delay (-d) is not a number!"
+			exit 1
+		fi
+		;;
+	*)
+		/bin/echo -e "Unknown argument: -$OPTARG\n"
+		/bin/echo "Usage: autobackup.sh [-q] [-d DELAY] <DEVICE>"
+		/bin/echo -e '\nWhere DEVICE is a mountable device and DELAY the time this script should'\
+			     '\nwait before it starts the backup in seconds. With -q you can supress the'\
+			     '\nbeep sound that is normally played and the dialog for an error or success.\n'
+		inform_error "Unknown argument: -$OPTARG"
+		exit 1
+		;;
+  esac
+done
+shift $((OPTIND-1))
+echo $1
+		
 
 if [ ! $1 ]
 then
-	/bin/echo "Error, no backup device specified!"
-	$BEEP_ERROR
+	inform_error "Error, no backup device specified!"
 	exit 1
 fi
 
 if [ ! -b $1 ]
 then
-	/bin/echo "Error, specified device does not exist!"
-	$BEEP_ERROR
+	inform_error "Error, specified device does not exist!"
 	exit 1
-fi
-
-if [ $2 ]
-then
-	if [[ $2 =~ ^[0-9]+ ]]
-	then
-		/bin/echo "Wait for $2 seconds ..."
-		/bin/sleep $2
-	else
-		/bin/echo "Error, specified delay is not a number!"
-		$BEEP_ERROR
-		exit 1
-	fi
 fi
 
 /bin/echo "Mounting $1 to /media/backup..."
@@ -85,18 +127,8 @@ then
     then
         /bin/echo "Login to efa2 server failed, please check that the efaLive administrator is created in efa2 configuration"
     fi
-    /bin/echo "Error, backup failed!"
-    $BEEP_ERROR
-    if [ "x$AUTO_USB_BACKUP_DIALOG" = "xTRUE" ]
-    then
-        /usr/bin/zenity --error --text="Backup failed, error code: $BACKUP_RESULT !\n\nView autobackup.log for details."
-    fi
+    inform_error "Backup failed, error code: $BACKUP_RESULT !\n\nView autobackup.log for details."
 else
-    /bin/echo "Backup successful."
-    $BEEP_SUCCESS
-    if [ "x$AUTO_USB_BACKUP_DIALOG" = "xTRUE" ]
-    then
-        /usr/bin/zenity --info --text="Backup successful."
-    fi
+    inform_success "Backup successful."
 fi
 exit $BACKUP_RESULT
