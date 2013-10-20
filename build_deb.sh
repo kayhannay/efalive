5f7de054b8de990d214e607707f0e5031e3521f7
Kay Hannay <klinux@hannay.de>
2013-10-20 18:36:30 +0200

    Update build script

--- build_deb.sh
+++ build_deb.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 #
 #
-VERSION=2.2
+#VERSION=2.2
 
-rm -r efalive-*
-mkdir efalive-$VERSION
+#rm -r efalive-*
+#mkdir efalive-$VERSION
 #cp -a content/* efalive-$VERSION
-cp -a debian efalive-$VERSION
-cd content
-tar czf ../efalive_$VERSION.orig.tar.gz *
-cd ..
-cd efalive-$VERSION
-debuild -uc -us
-cd ..
+#cp -a debian efalive-$VERSION
+#cd content
+#tar czf ../efalive_$VERSION.orig.tar.gz *
+#cd ..
+#cd efalive-$VERSION
+#debuild -uc -us
+#cd ..
 
+BUILD_DIR=build/efalive
+PROJECT_ROOT=$(pwd)
+
+mkdir -p $BUILD_DIR
+cp -r debian $BUILD_DIR
+
+cd src/python
+python setup.py sdist --dist-dir ../../build/python
+cd $PROJECT_ROOT
+
+cd $BUILD_DIR
+debuild -uc -us -b
+
+cd $PROJECT_ROOT
